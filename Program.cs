28cdc5d5ea133ad4a7579455208aa45a44ec247b
DHemken <DHemken@wardkraft.com>
2020-12-10 18:28:01 -0600

    Add project files.

---
+++ Program.cs
@@ -0,0 +1,26 @@
+﻿using System;
+using TransactionalEntities;
+
+namespace TransactionalEntityPOC
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Config.Init();
+            Config.connectionString = "Data Source=LocalHost;Initial Catalog=Test;Integrated Security=True";
+
+
+            var t = new MyType {Value1 = "test2", Id = Guid.NewGuid()};
+            t.Commit();
+
+        }
+    }
+   
+    public class MyType : Entity
+    {
+        public string Value1 { get; set; }
+
+    }
+   
+}
